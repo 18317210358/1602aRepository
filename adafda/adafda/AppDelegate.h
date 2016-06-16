899788ea2de9cc61db1d56ed51400aba46db9daa
18317210358 <455576376@qq.com>
2016-06-16 11:22:37 +0800

    dsaldjalf

---
+++ adafda/adafda/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  adafda
+//
+//  Created by Apple on 16/6/16.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ adafda/adafda/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  adafda
+//
+//  Created by Apple on 16/6/16.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
